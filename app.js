a985794c4c4462a88d4aae16116c6ea9a0151f41
myrmnn <myrm.nn@gmail.com>
2020-12-27 07:30:46 -0500

    added background color and rotation to arrows

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const question = document.querySelectorAll('.question');
-const answer = document.querySelectorAll('.answer');
+const question = document.querySelectorAll(".question");
+const answer = document.querySelectorAll(".answer");
+const arrow = document.querySelector(".arrow");
 
 question.forEach((el) =>
-	el.addEventListener('click', () => {
-		el.classList.toggle('bold');
-		el.nextElementSibling.classList.toggle('open');
-	})
+  el.addEventListener("click", () => {
+    el.classList.toggle("bold");
+    el.nextElementSibling.classList.toggle("open");
+    el.firstElementChild.classList.toggle("rotate");
+  })
 );
